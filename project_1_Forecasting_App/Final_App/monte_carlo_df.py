74da2f709ece72f5548d20e4bd7fe83f9c755202
Sam Eberts <sam.eberts12@gmail.com>
2022-07-21 11:56:06 -0700

    Update monte_carlo_df.py
    Added final comments.

--- project_1_Forecasting_App/Final_App/monte_carlo_df.py
+++ project_1_Forecasting_App/Final_App/monte_carlo_df.py
@@ -12,10 +12,17 @@ from find_portfolio_weights import portfolio_weights
 from MCForecastTools import MCSimulation
 
 
+# Creating a function to create a dataframe that fits the specific format of the monte carlo simulation 
 def create_simulation_df(df_portfolio):
+    # creating an empty dictionary
     test = {}
+    # Using a for loop to iterate through each stock symbol in the created alpaca dataframe 
     for ticker in df_portfolio['symbol'].unique():
+        # Creating a key:value pair for the ticker and its data to seperate the dataframe
         test[ticker] = copy.deepcopy(df_portfolio.loc[df_portfolio['symbol'] == ticker])
+        # Dropping the symbol column from each dataframe
         test[ticker].drop(columns = ['symbol'], inplace = True)
+    # Using pd.concat to concatenate the seperated data frames 
+    # using a for loop to iterate through the key in test dictionary
     testing_df = pd.concat([test[key] for key in test.keys()], axis=1, keys=test.keys())
     return testing_df
