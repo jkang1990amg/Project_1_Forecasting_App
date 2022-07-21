8938637a14689bd9854d90fe1c613ff2cc4542c9
Sam Eberts <sam.eberts12@gmail.com>
2022-07-21 11:37:50 -0700

    Update api_caller.py
    Added final comments to the code.

--- project_1_Forecasting_App/Final_App/api_caller.py
+++ project_1_Forecasting_App/Final_App/api_caller.py
@@ -4,19 +4,23 @@ from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 
 
-
+# Creating a function called api_call()
 def api_call():
+    # Checking to see if the computer recognizes the env file in the working directory 
     load_dotenv()
     
+    # Getting the API key and Secret Key
     alpaca_api = os.getenv("ALPACA_API_KEY")
     alpaca_secret = os.getenv("ALPACA_SECRET_KEY")
     
+    # Creating the rest object
     alpaca = tradeapi.REST(
         alpaca_api,
         alpaca_secret,
         api_version="v2"
     )
     
+    # returning the rest object
     return alpaca
 
 
